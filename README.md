3ba9e76a0525b7cd1728ad33def263df566d866f
Bryan Sng <hong.sng@ucdconnect.ie>
2020-09-02 00:45:20 +0100

    Updated with things that occured in stage 3 and 2020

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ A personal website aimed to facilitate the function of a resume or CV, with ease
 &nbsp;
 ## Characteristics
 - Content of website is located in content.json. Editing is just as simple as editing the json file. ~~(could have used markdown to json, makes editing easier)~~
-- Project's cards are based on GitHub's pinned repositories. Repo data is fetched using GraphQL. ~~(scales only if read-only OAuth tokens are allowed in public GitHub repos. which they don't. Can be fixed by fetching data from a backend server that hides the token)~~
+- Project's cards are based on GitHub's pinned repositories. Repo data is fetched using GraphQL. Additional notable repos can be specified in content.json. ~~(scales only if read-only OAuth tokens are allowed in public GitHub repos. which they don't. Can be fixed by fetching data from a backend server that hides the token)~~
 - Theme design is based on [Visual Studio Code's Dark Plus](https://github.com/microsoft/vscode/tree/master/extensions/theme-defaults/themes).
 
 &nbsp;
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,22 +1,26 @@
-import React from 'react';
-import SocialMedia from './SocialMedia/SocialMedia';
-import content from '../config/content';
+import React from "react";
+import SocialMedia from "./SocialMedia/SocialMedia";
+import content from "../config/content";
 
 const Footer = () => {
-	// const currentYear = new Date().getFullYear();
-	const { github_link_msg, github_link_url } = content.footer;
-	return (
-		<div className="footer-bg shadow-2">
-			<SocialMedia />
-			<div className="tc ma0 pt2 pb4 pv4-l">
-				<a href={github_link_url} className="no-underline">
-					<span className="ph2 font-ubuntu-mono footer transition-ease-in">
-						{github_link_msg}
-					</span>
-				</a>
-			</div>
-		</div>
-	)
-}
+  // const currentYear = new Date().getFullYear();
+  const { github_link_msg, github_link_url, last_updated } = content.footer;
+  return (
+    <div className="footer-bg shadow-2">
+      <SocialMedia />
+      <div className="tc ma0 pt2 pb4 pv4-l font-ubuntu-mono">
+        <a
+          href={github_link_url}
+          className="pa1 no-underline footer transition-ease-in"
+        >
+          {github_link_msg}
+        </a>
+        <div className="pt3 f6 no-underline footer-no-hover transition-ease-in">
+          Last Updated: {last_updated}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Footer;
